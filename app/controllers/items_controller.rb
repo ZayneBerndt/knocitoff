4d4e24e842795e4fde28a6326138a787c22803bb
Zayne <berndt.zayne@gmail.com>
2015-10-05 23:02:18 +1300

    Clean-up CRUD

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,57 @@
 class ItemsController < ApplicationController
+ 
+  def new 
+    @item = @Item.new 
+  end 
+
+
   def create
     @user = current_user
-    @new_item = Item.new 
-    @item = current_user.items.build(item_params)
+    @item = current_user.items.build(params[:id])
+    if @item.save 
+      flash[:notice] = "Item created succesfully"
+      
+    else 
+      flash[:error] = "Something went wrong. Please try again."
+        render :new # Draws new action again. 
   end
+end 
+
 
   def destroy
     @item = Item.find(params[:id])
- end
+    if @item.destroy 
+      flash[:notice] =  "\"#{@item.name}\" was deleted succesfully."
+      
+    else 
+      flash[:error] = "There was an error deleting your item"
+        render :show
+    end 
+  end 
+
+
+ def show 
+
+ end 
+
+
+ def edit 
+ end 
+
+  def update
+    #load message from params 
+    if @item.update_attributes(message_params)
+      #load flash 
+      #redirect message 
+    else 
+      #load flash 
+      #render action we came from 
+    end 
+  end 
 
  private 
 
  def item_params 
   params.require(:item).permit(:name)
-end
-
+ end
 end 
